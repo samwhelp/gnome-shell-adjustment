17f62bc935e9166c5c43bab850c7d0cd083ab9f8
samwhelp <samwhelp@users.noreply.github.com>
2026-02-13 12:09:05 +0800

    fix

--- demo/scripts/main/gnome-shell-adjustment/gnome-shell-adjustment.sh
+++ demo/scripts/main/gnome-shell-adjustment/gnome-shell-adjustment.sh
@@ -167,6 +167,34 @@ sys_gnome_shell_main_config_install_for_main () {
 dconf load / << __EOF__
 
 
+[org/gnome/desktop/wm/preferences]
+action-double-click-titlebar='toggle-maximize'
+action-middle-click-titlebar='toggle-shade'
+action-right-click-titlebar='menu'
+button-layout='appmenu:minimize,maximize,close'
+mouse-button-modifier='<Super>'
+resize-with-right-button=true
+raise-on-click=true
+focus-mode='click'
+focus-new-windows='smart'
+
+
+[org/gnome/desktop/peripherals/keyboard]
+numlock-state=false
+remember-numlock-state=true
+
+
+[org/gnome/desktop/sound]
+event-sounds=false
+
+
+[org/gnome/desktop/interface]
+clock-format='24h'
+clock-show-seconds=true
+clock-show-weekday=true
+enable-hot-corners=true
+
+
 __EOF__
 
 }
@@ -174,6 +202,15 @@ __EOF__
 sys_gnome_shell_main_config_install_for_workspace_main () {
 
 dconf load / << __EOF__
+
+
+[org/gnome/desktop/wm/preferences]
+num-workspaces=5
+workspace-names=['File', 'Edit', 'Web', 'Term', 'Misc']
+
+
+[org/gnome/mutter]
+dynamic-workspaces=false
 
 
 __EOF__
@@ -185,6 +222,48 @@ sys_gnome_shell_main_config_install_for_favorite_apps () {
 dconf load / << __EOF__
 
 
+[org/gnome/shell]
+#favorite-apps=['org.gnome.Nautilus.desktop', 'org.gnome.TextEditor.desktop', 'firefox.desktop', 'org.gnome.Console.desktop', 'org.gnome.Settings.desktop', 'org.gnome.tweaks.desktop', 'org.gnome.Extensions.desktop', 'com.mattjakeman.ExtensionManager.desktop', 'org.gnome.Software.desktop']
+#favorite-apps=['thunar.desktop', 'sublime_text.desktop', 'firefox.desktop', 'xfce4-terminal.desktop', 'org.gnome.Settings.desktop', 'org.gnome.tweaks.desktop', 'org.gnome.Extensions.desktop', 'com.mattjakeman.ExtensionManager.desktop', 'org.gnome.Software.desktop']
+favorite-apps=['org.gnome.Nautilus.desktop', 'org.gnome.TextEditor.desktop', 'firefox-esr.desktop', 'org.gnome.Ptyxis.desktop', 'org.gnome.Settings.desktop', 'org.gnome.tweaks.desktop', 'org.gnome.Extensions.desktop', 'com.mattjakeman.ExtensionManager.desktop', 'org.gnome.Software.desktop']
+
+
+
+
+
+
+
+
+##
+## ## Terminal / Sample
+##
+
+#[org/gnome/desktop/default-applications.terminal]
+#exec='xdg-terminal-exec'
+#exec-arg='--'
+
+
+
+
+##
+## ## Terminal / Use Case
+##
+
+[org/gnome/desktop/default-applications.terminal]
+exec='ptyxis'
+exec-arg=''
+
+
+[org/mate/applications-terminal]
+exec='ptyxis'
+exec-arg=''
+
+
+[org/cinnamon/desktop/default-applications.terminal]
+exec='ptyxis'
+exec-arg=''
+
+
 __EOF__
 
 }
@@ -194,6 +273,22 @@ sys_gnome_shell_main_config_install_for_theme_main () {
 dconf load / << __EOF__
 
 
+[org/gnome/desktop/interface]
+color-scheme='prefer-dark'
+gtk-theme='Adwaita-dark'
+icon-theme='Papirus-Dark'
+cursor-theme='Adwaita'
+cursor-size=24
+
+
+[org/gnome/desktop/wm/preferences]
+theme='Adwaita-dark'
+
+
+[org/gnome/shell/extensions/user-theme]
+name='Adwaita-dark'
+
+
 __EOF__
 
 }
@@ -201,6 +296,16 @@ __EOF__
 sys_gnome_shell_main_config_install_for_wallpaper_main () {
 
 dconf load / << __EOF__
+
+
+[org/gnome/desktop/background]
+picture-options='zoom'
+picture-uri='file:///usr/share/backgrounds/default.jpg'
+picture-uri-dark='file:///usr/share/backgrounds/default.jpg'
+
+
+[org/gnome/desktop/screensaver]
+picture-uri='file:///usr/share/backgrounds/default-login.jpg'
 
 
 __EOF__
